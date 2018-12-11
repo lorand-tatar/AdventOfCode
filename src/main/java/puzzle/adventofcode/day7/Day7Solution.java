c6a5784569352959d4acf13e5a9df29c8f9c6022
Lorand Tatar <Lorand_Tatar@epam.com>
2018-12-11 18:31:53 +0100

    day7: almost there, just need to consider if all prerequisites are done for a task to be ready

--- src/main/java/puzzle/adventofcode/day7/Day7Solution.java
+++ src/main/java/puzzle/adventofcode/day7/Day7Solution.java
@@ -1,16 +1,19 @@
 package puzzle.adventofcode.day7;
 
-import puzzle.adventofcode.Solution;
+import static java.nio.file.Files.readAllLines;
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static java.nio.file.Files.readAllLines;
-import static java.util.stream.Collectors.toList;
+import puzzle.adventofcode.Solution;
+import puzzle.adventofcode.day7.TaskGraph.GraphNode;
 
 public class Day7Solution implements Solution {
 
@@ -19,22 +22,63 @@ public class Day7Solution implements Solution {
     @Override
     public void run() {
         try {
-            var rawInstructions = readAllLines(Path.of("C:/Users/Flori/IdeaProjects/AdventOfCode/src/main/resources/puzzle7a_input.txt"));
+            var rawInstructions = readAllLines(Path.of("C:/stash/AdventOfCode/src/main/resources/puzzle7a_input.txt"));
 
             List<Prerequisite> prerequisites = rawInstructions.stream()
-                    .map(instruction -> {
-                        Matcher instructionMatcher = INSTRUCTION_PATTERN.matcher(instruction);
-                        instructionMatcher.lookingAt();
-                        return new Prerequisite(instructionMatcher.group(1).charAt(0), instructionMatcher.group(2).charAt(0));
-                    })
-                    .collect(toList());
+                .map(instruction -> {
+                    Matcher instructionMatcher = INSTRUCTION_PATTERN.matcher(instruction);
+                    instructionMatcher.lookingAt();
+                    return new Prerequisite(instructionMatcher.group(1).charAt(0), instructionMatcher.group(2).charAt(0));
+                })
+                .collect(toList());
 
             var graph = new TaskGraph(new HashSet<>());
-            
+            buildTaskGraph(prerequisites, graph);
+            var startCandidates = graph.getNodes().stream()
+                .filter(node -> node.getDependencies().isEmpty())
+                .collect(toList());
+            startCandidates.sort(comparing(GraphNode::getTask));
+            var endNodes = graph.getNodes().stream()
+                .filter(node -> node.getFollowUps().isEmpty())
+                .collect(toList());
+            // O P V starter, Q ending
+            var processableNodes = startCandidates;
+            var taskSequence = "";
+            var doneSteps = new HashSet<GraphNode>();
+            while (!processableNodes.isEmpty()) {
+                final GraphNode expandedNode = processableNodes.get(0);
+                taskSequence = taskSequence.concat(String.valueOf(expandedNode.getTask()));
+                doneSteps.add(expandedNode);
+                processableNodes.addAll(expandedNode.getFollowUps());
+                System.out.println(processableNodes.size());
+                var intermediateSet = new HashSet<GraphNode>();
+                intermediateSet.addAll(processableNodes);
+                intermediateSet.removeAll(doneSteps);
+                System.out.println(processableNodes.size());
+                processableNodes = new ArrayList<GraphNode>(intermediateSet);
+                System.out.println(processableNodes.size());
+                processableNodes.sort(comparing(GraphNode::getTask));
+            }
+            System.out.println(taskSequence);
         } catch (IOException e) {
             System.out.println("Can't read file!");
             System.out.println(e);
         }
+    }
+
+    private void buildTaskGraph(List<Prerequisite> prerequisites, TaskGraph graph) {
+        prerequisites.stream()
+            .peek(prerequisite -> {
+                var nodes = graph.getNodes();
+                nodes.add(new GraphNode(prerequisite.getPrerequisite()));
+                nodes.add(new GraphNode(prerequisite.getOfThisOne()));
+            })
+            .forEach(prerequisite -> {
+                var leftNode = graph.lookUpNodeForTask(prerequisite.getPrerequisite());
+                var rightNode = graph.lookUpNodeForTask(prerequisite.getOfThisOne());
+                leftNode.addFollowUp(rightNode);
+                rightNode.addDependency(leftNode);
+            });
     }
 
     private class Prerequisite {
--- src/main/java/puzzle/adventofcode/day7/TaskGraph.java
+++ src/main/java/puzzle/adventofcode/day7/TaskGraph.java
@@ -1,5 +1,9 @@
 package puzzle.adventofcode.day7;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class TaskGraph {
@@ -13,6 +17,13 @@ public class TaskGraph {
         return nodes;
     }
 
+    public GraphNode lookUpNodeForTask(Character task) {
+        return nodes.stream()
+            .filter(node -> node.getTask().equals(task))
+            .findFirst()
+            .get();
+    }
+
     @Override
     public String toString() {
         return "TaskGraph{" +
@@ -20,13 +31,15 @@ public class TaskGraph {
                 '}';
     }
 
-    private class GraphNode {
+    public static class GraphNode {
         private Character task;
         private Set<GraphNode> dependencies;
         private Set<GraphNode> followUps;
 
         public GraphNode(Character task) {
             this.task = task;
+            dependencies = new HashSet<>();
+            followUps = new HashSet<>();
         }
 
         public GraphNode(Character task, Set<GraphNode> dependencies, Set<GraphNode> followUps) {
@@ -47,12 +60,35 @@ public class TaskGraph {
             return followUps;
         }
 
+        public void addDependency(GraphNode dependency) {
+            dependencies.add(dependency);
+        }
+
+        public void addFollowUp(GraphNode followUp) {
+            followUps.add(followUp);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            GraphNode graphNode = (GraphNode) o;
+            return task.equals(graphNode.task);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(task);
+        }
+
         @Override
         public String toString() {
             return "GraphNode{" +
                     "task=" + task +
-                    ", dependencies=" + dependencies +
-                    ", followUps=" + followUps +
+                    ", dependencies=" + dependencies.stream().map(GraphNode::getTask).collect(toList()) +
+                    ", followUps=" + followUps.stream().map(GraphNode::getTask).collect(toList()) +
                     '}';
         }
     }
